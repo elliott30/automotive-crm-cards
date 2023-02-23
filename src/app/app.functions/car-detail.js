756fe6790a1371d6cee28cc21e3931e43982d9ae
Elliott Chapman <echapman@hubspot.com>
2023-02-23 22:00:24 +1300

    Sync with production build

--- src/app/app.functions/car-detail.js
+++ src/app/app.functions/car-detail.js
@@ -6,11 +6,72 @@ exports.main = async (context = {}, sendResponse) => {
     sendResponse({
       sections: [
         {
-          "type": "image",
-          "src": "https://www.ui-extensibility.com/hubfs/welcome-sign.png",
-          "alt": "A Welcome Sign sample image",
-        },
-      ],
-    });
+          "type": "tile",
+          "content": [
+          {
+            "type": "heading",
+            "text": "Car Details"
+          },
+          {
+            "type": "tag",
+            "text": "Purchased",
+            "variant": "success",
+          },
+          {
+            "type": "text",
+            "format": "markdown",
+            "text": "**Car: ** 541774"
+          },
+          
+          {
+            "type": "text",
+            "format": "markdown",
+            "text": "**Description: **Matching number xw gt candy apple red with black interior factory manual. Exceptional sedan in the flesh. All original matching motor, gearbox, diff, fuel lines etc come with this vehicle. "
+          },
+          {
+            "type": "button",
+            "variant": "primary",
+            "text": "View in Titan"
+          },
+        ]
+      },
+      {
+        "type": "tile",
+        "content": [
+          {
+            "type": "heading",
+            "text": "Car Images"
+          },
+          {
+            "type": "image",
+            "src": "https://23786408.fs1.hubspotusercontent-na1.net/hubfs/23786408/car-1.jpg",
+            "alt": "Car",
+          },
+          {
+            "type": "buttonRow",
+            "buttons": [
+              {
+                "type": "button",
+                "variant": "primary",
+                "text": "1"
+              },
+              {
+                "type": "button",
+                "text": "2"
+              },
+              {
+                "type": "button",
+                "text": "3"
+              },
+              {
+                "type": "button",
+                "text": "4"
+              }
+            ]
+          }
+      ]
+    }
+  ],
+});
  
 };
